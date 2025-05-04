dc2207167d360b65c5da0069984f3a31dc23ea73
gab <gabriel.pontarolo045@gmail.com>
2025-05-04 12:52:53 -0300

    Make RVR generic and update interface.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,71 @@
 import rvr
+import os
 
-NUM_ROUNDS = 50
+NUM_ROUNDS = 140
+
+DEFFENSIVE_REWARDS = {
+    "Block": {"Throw": -1.7, "Delayed_tc": -0.7, "Meaty_high": -0.7, "Meaty_low": -0.5, "Shimmy_lp": -0.5},
+    "Guard_jump": {"Throw": 0.5, "Delayed_tc": -4.2, "Meaty_high": -0.7, "Meaty_low": -0.5, "Shimmy_lp": 0.5},
+    "Button": {"Throw": -1.7, "Delayed_tc": 4.2, "Meaty_high": -4.2, "Meaty_low": -3.2, "Shimmy_lp": 3.7},
+    "Throw": {"Throw": -0.5, "Delayed_tc": 1.7, "Meaty_high": -4.2, "Meaty_low": -3.2, "Shimmy_lp": -4.2},
+    "Reversal": {"Throw": 2.1, "Delayed_tc": -3.7, "Meaty_high": 2.1, "Meaty_low": 2.1, "Shimmy_lp": -4.2},
+    "Parry_high": {"Throw": -1.7, "Delayed_tc": 4.2, "Meaty_high": 4.2, "Meaty_low": -3.2, "Shimmy_lp": -0.5},
+    "Parry_low": {"Throw": -1.7, "Delayed_tc": -4.2, "Meaty_high": -4.2, "Meaty_low": 4.2, "Shimmy_lp": 0.5},
+}
+
+OFFENSIVE_REWARDS = {
+    "Throw": {"Block": 1.7, "Guard_jump": -0.5, "Button": 1.7, "Throw": 0.7, "Reversal": -2.1, "Parry_high": 1.7, "Parry_low": 1.7},
+    "Delayed_tc": {"Block": 0.7, "Guard_jump": 4.2, "Button": -4.2, "Throw": -1.7, "Reversal": 3.7, "Parry_high": -4.2, "Parry_low": 4.2},
+    "Meaty_high": {"Block": 0.7, "Guard_jump": 0.7, "Button": 4.2, "Throw": 4.2, "Reversal": -2.2, "Parry_high": -4.2, "Parry_low": 4.2},
+    "Meaty_low": {"Block": 0.5, "Guard_jump": 0.5, "Button": 3.2, "Throw": 3.2, "Reversal": -2.2, "Parry_high": 3.2, "Parry_low": -4.2},
+    "Shimmy_lp": {"Block": 0.5, "Guard_jump": -0.5, "Button": -3.7, "Throw": 3.7, "Reversal": 4.2, "Parry_high": 0.5, "Parry_low": 0.5},
+}
+
+def get_player_action(prompt, possible_actions):
+    print("Actions:")
+    for i, action in enumerate(possible_actions):
+        print(f"{i}: {action}", end="   ")
+    print()
+
+    input_str = input(prompt)
+
+    # Check if input is a number in range
+    if input_str.isdigit():
+        try:
+            action_index = int(input_str)
+            if 0 <= action_index < len(possible_actions):
+                return possible_actions[action_index]
+            else:
+                print("Invalid input. Please enter a valid action number.")
+                return get_player_action(prompt, possible_actions)
+        except ValueError:
+            print("Invalid input. Please enter a valid action number.")
+            return get_player_action(prompt, possible_actions)
 
 if __name__ == "__main__":
 
-    ai = rvr.AI()
+    defensive_ai = rvr.AI(DEFFENSIVE_REWARDS)
+    offensive_ai = rvr.AI(OFFENSIVE_REWARDS)
 
     ai_wins, player_wins = 0, 0
 
     for r in range(NUM_ROUNDS):
+        os.system("cls" if os.name == "nt" else "clear")
+
         print("\n=====================")
         print(f"Round {r + 1}/{NUM_ROUNDS} - AI wins: {ai_wins}, Your wins: {player_wins}\n")
         print("You're attacking...")
-        print("Actions:")
-        ai.list_offense_actions()
         
-        pred_offense, ai_defense = ai.predict_offense_action()
-        player_offense = ai.get_offense_action(int(input("")))
+        # Call AI to predict player action and a response
+        pred_offense, ai_defense = defensive_ai.predict_action()
+        player_offense = get_player_action("Choose your action: ", defensive_ai.get_player_actions())
         
         print(f"Your offensive action: {player_offense}\n")
         print(f"AI predicted possible offensive actions: {pred_offense}")
         print(f"AI defensive action: {ai_defense}\n")
 
-        player_reward, ai_reward = ai.calculate_rewards(player_offense, ai_defense)
+        # Calculate rewards
+        player_reward, ai_reward = defensive_ai.calculate_rewards(player_offense, ai_defense)
         print(f"Your reward: {player_reward}")
         print(f"AI reward: {ai_reward}\n")
         
@@ -34,18 +77,32 @@ if __name__ == "__main__":
             print("AI")
             ai_wins += 1
 
-        ai.update_offense(player_offense)
+        defensive_ai.update(player_offense)
 
-        # print("You're defending...")
-        # pred_defense, ai_offense = ai.predict_defense_action()
-        # player_defense = ai.get_defense_action(input())
+        print("\n=====\n")
+        print("You're defending...")
 
-        # print(f"Your defensive action: {player_defense}\n")
-        # print(f"AI predicted defensive action: {pred_defense}")
-        # print(f"AI offensive action: {ai_offense}\n")
+        # Call AI to predict player action and a response
+        pred_defense, ai_offense = offensive_ai.predict_action()
+        player_defense = get_player_action("Choose your action: ", offensive_ai.get_player_actions())
+        
+        print(f"Your defensive action: {player_defense}\n")
+        print(f"AI predicted possible defensive actions: {pred_defense}")
+        print(f"AI offensive action: {ai_offense}\n")
+        
+        # Calculate rewards
+        player_reward, ai_reward = offensive_ai.calculate_rewards(player_defense, ai_offense)
+        print(f"Your reward: {player_reward}")
+        print(f"AI reward: {ai_reward}\n")
 
-        # ai_reward, player_reward = ai.calculate_rewards(ai_offense, player_defense)
-        # print(f"Your reward: {player_reward}")
-        # print(f"AI reward: {ai_reward}\n")
-        # print(f"Winner: {'You' if player_reward > ai_reward else 'AI'}\n\n")
-        # ai.update_defense(player_defense)
+        print("Winner: ", end="")
+        if player_reward > ai_reward:
+            print("You")
+            player_wins += 1
+        elif player_reward < ai_reward:
+            print("AI")
+            ai_wins += 1
+        
+        offensive_ai.update(player_defense)
+
+        input("\n=====================\n")
--- src/rvr.py
+++ src/rvr.py
@@ -1,74 +1,45 @@
 import markov
 
-DEFFENSIVE_REWARDS = {
-    "Block": {"Throw": -1.7, "Delayed_tc": -0.7, "Meaty_high": -0.7, "Meaty_low": -0.5, "Shimmy_lp": -0.5},
-    "Guard_jump": {"Throw": 0.5, "Delayed_tc": -4.2, "Meaty_high": -0.7, "Meaty_low": -0.5, "Shimmy_lp": 0.5},
-    "Button": {"Throw": -1.7, "Delayed_tc": 4.2, "Meaty_high": -4.2, "Meaty_low": -3.2, "Shimmy_lp": 3.7},
-    "Throw": {"Throw": -0.5, "Delayed_tc": 1.7, "Meaty_high": -4.2, "Meaty_low": -3.2, "Shimmy_lp": -4.2},
-    "Reversal": {"Throw": 2.1, "Delayed_tc": -3.7, "Meaty_high": 2.1, "Meaty_low": 2.1, "Shimmy_lp": -4.2},
-    "Parry_high": {"Throw": -1.7, "Delayed_tc": 4.2, "Meaty_high": 4.2, "Meaty_low": -3.2, "Shimmy_lp": -0.5},
-    "Parry_low": {"Throw": -1.7, "Delayed_tc": -4.2, "Meaty_high": -4.2, "Meaty_low": 4.2, "Shimmy_lp": 0.5},
-}
-
-OFFENSIVE_REWARDS = {
-    "Throw": {"Block": 1.7, "Guard_jump": -0.5, "Button": 1.7, "Throw": 0.7, "Reversal": -2.1, "Parry_high": 1.7, "Parry_low": 1.7},
-    "Delayed_tc": {"Block": 0.7, "Guard_jump": 4.2, "Button": -4.2, "Throw": -1.7, "Reversal": 3.7, "Parry_high": -4.2, "Parry_low": 4.2},
-    "Meaty_high": {"Block": 0.7, "Guard_jump": 0.7, "Button": 4.2, "Throw": 4.2, "Reversal": -2.2, "Parry_high": -4.2, "Parry_low": 4.2},
-    "Meaty_low": {"Block": 0.5, "Guard_jump": 0.5, "Button": 3.2, "Throw": 3.2, "Reversal": -2.2, "Parry_high": 3.2, "Parry_low": -4.2},
-    "Shimmy_lp": {"Block": 0.5, "Guard_jump": -0.5, "Button": -3.7, "Throw": 3.7, "Reversal": 4.2, "Parry_high": 0.5, "Parry_low": 0.5},
-}
-
 MARKOV_ORDER = 5
 
 class AI():
 
-    def __init__(self) -> None:
+    def __init__(self, rvr) -> None:
 
-        self.offensive_actions = list(OFFENSIVE_REWARDS.keys())
-        self.offsentive_pred_chain = markov.MarkovChain(MARKOV_ORDER, self.offensive_actions)
+        self.rewards = rvr
 
-        self.deffensive_actions = list(DEFFENSIVE_REWARDS.keys())
-        self.deffensive_pred_chain = markov.MarkovChain(MARKOV_ORDER, self.deffensive_actions)
+        self.ai_actions = list(rvr.keys())
+        self.player_actions = list(rvr[self.ai_actions[0]].keys())
+        self.multi_markov = markov.MultiMarkovChain(MARKOV_ORDER, self.player_actions)
 
-    def list_defense_actions(self):
-        for i, action in enumerate(self.deffensive_actions):
-            print(f"{i}: {action}", end=", ")
-        print("")
+    def get_player_actions(self):
+        return self.player_actions
 
-    def list_offense_actions(self):
-        for i, action in enumerate(self.offensive_actions):
-            print(f"{i}: {action}", end=", ")
-        print(" ")
-
-    def predict_offense_action(self):
-        possible_off_actions = self.offsentive_pred_chain.predict()
+    def predict_action(self):
+        possible_player_actions = self.multi_markov.predict()
         
-        # For each def action, weight the reward with the probability of the off action
-        defensive_actions = {}
-        for def_action in self.deffensive_actions:
+        # For each AI action, weight the reward with the probability of the respective possible player action
+        possible_ai_actions = {}
+        for ai_action in self.ai_actions:
             weight = 0.0
-            for off_action in possible_off_actions:
-                weight += DEFFENSIVE_REWARDS[def_action][off_action[0]] * off_action[1]["prob"]
-            defensive_actions[def_action] = weight
+            for player_action in possible_player_actions:
+                weight += self.rewards[ai_action][player_action[0]] * player_action[1]
+            possible_ai_actions[ai_action] = weight
 
-        # Sort the defensive actions by their reward
-        defensive_actions = sorted(defensive_actions.items(), key=lambda action: action[1], reverse=True)
+        # Sort the AI actions according to their reward
+        possible_ai_actions = sorted(possible_ai_actions.items(), key=lambda action: action[1], reverse=True)
 
-        print(f"\n  [DEBUG] Finished prediction")
-        print(f"    [DEBUG] Possible offensive actions: {possible_off_actions}")
-        print(f"    [DEBUG] Possible defensive actions: {defensive_actions}\n")
+        # print(f"\n    [DEBUG] Finished prediction")
+        # print(f"    [DEBUG] Possible offensive actions: {possible_player_actions}")
+        # print(f"    [DEBUG] Possible defensive actions: {possible_ai_actions}\n")
 
-        return [a[0] for a in possible_off_actions], defensive_actions[0][0]
+        return [a[0] for a in possible_player_actions], possible_ai_actions[0][0]
     
-    def get_offense_action(self, idx):
-        return self.offensive_actions[idx]
-    
-    def update_offense(self, action):
-        self.offsentive_pred_chain.update_matrix(action)
+    def update(self, action):
+        self.multi_markov.update_matrix(action)
 
-    def calculate_rewards(self, offense, defense):
-        offensive = OFFENSIVE_REWARDS[offense][defense] 
-        defensive = DEFFENSIVE_REWARDS[defense][offense]
+    def calculate_rewards(self, player_action, ai_action):
+        ai_reward = self.rewards[ai_action][player_action]
 
-        return offensive, defensive
+        return -ai_reward, ai_reward
         
